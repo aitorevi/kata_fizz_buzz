fadc18c97dba1f0f0c5d991d128c1e130278ebec
aitorevi <aitorevi@gmail.com>
2023-03-08 00:25:10 +0100

    Before new FizzBuzzTDD

---
+++ src/main/java/kata/example/FizzBuzzTDD.java
@@ -0,0 +1,4 @@
+package kata.example;
+
+public class FizzBuzzTDD {
+}
---
+++ src/test/java/kata/example/FizzBuzzTDDTest.java
@@ -0,0 +1,21 @@
+package kata.example;
+
+/*
+ * 1 -> 1
+ * 3 -> Fizz
+ * 5 -> Buzz
+ * 15 -> FizzBuzz
+ * number % 3 == 0 -> Fizz
+ * number % 5 == 0 -> Buzz
+ * number % 15 == 0 -> FizzBuzz
+ */
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTDDTest {
+    @Test
+    void should_return_one_as_is_one() {
+
+    }
+
+}
