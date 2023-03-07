7d290db25e7128a7d558c541c58e779b2b6503a6
aitorevi <aitorevi@gmail.com>
2023-03-07 15:32:13 +0100

    After finish FizzBuzzWithObjects.java

--- src/main/java/kata/example/FizzBuzzWithObjects.java
+++ src/main/java/kata/example/FizzBuzzWithObjects.java
@@ -1,17 +1,97 @@
 package kata.example;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
+
+// Font: https://linuxtut.com/en/4eb551077595605d5e2e/
 
 public class FizzBuzzWithObjects {
     public static void main(String[] args) {
-        for ( int i=1 ; i <= 100 ; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            }else {
-                System.out.println(i);
-            }
-        }
+        Operator operator = new Operator();
+
+        Predicate<Integer> divisibleBy3 = DivisiblePredicateFactory.divisibleBy(3);
+        Predicate<Integer> divisibleBy5 = DivisiblePredicateFactory.divisibleBy(5);
+
+        Operation fizzbuzz = new Operation(new Specification(divisibleBy3).and(divisibleBy5), "FizzBuzz");
+        Operation fizz = new Operation(new Specification(divisibleBy3), "Fizz");
+        Operation buzz = new Operation(new Specification(divisibleBy5), "Buzz");
+
+        operator.addOperation(fizzbuzz);
+        operator.addOperation(fizz);
+        operator.addOperation(buzz);
+
+        operator.run(IntStream.rangeClosed(1, 100));
     }
 }
+class Operation {
+
+    private final Specification specification;
+    private final String message;
+
+    Operation(Specification specification, String message) {
+        this.specification = specification;
+        this.message = message;
+    }
+
+    public Specification getSpecification() {
+        return this.specification;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
+
+class Operator {
+
+    private final List<Operation> operationList = new ArrayList<>();
+
+    public void addOperation(Operation operation) {
+        this.operationList.add(operation);
+    }
+
+    public void run(IntStream range) {
+        range.forEach(number -> {
+            String message = this.operationList.stream()
+                    .filter(operation -> operation.getSpecification().isSatisfiedBy(number))
+                    .map(Operation::getMessage)
+                    .findFirst()
+                    .orElse(String.valueOf(number));
+            System.out.println(message);
+        });
+    }
+}
+
+class DivisiblePredicateFactory {
+
+    public static Predicate<Integer> divisibleBy(Integer divisor) {
+        return n -> n % divisor == 0;
+    }
+
+}
+
+class Specification {
+
+    private List<Predicate<Integer>> predicateList = new ArrayList<>();
+
+    public Specification(Predicate<Integer> predicate) {
+        this.predicateList.add(predicate);
+    }
+
+    private Specification(List<Predicate<Integer>> predicateList) {
+        this.predicateList = predicateList;
+    }
+
+    public Specification and(Predicate<Integer> predicate) {
+        List<Predicate<Integer>> results = new ArrayList<>(this.predicateList);
+        results.add(predicate);
+        return new Specification(results);
+    }
+
+    public boolean isSatisfiedBy(Integer number) {
+        return this.predicateList.stream().allMatch(p -> p.test(number));
+    }
+
+}
+
---
+++ src/test/java/kata/example/FizzBuzzWithObjectsTest.java
@@ -0,0 +1,45 @@
+package kata.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzWithObjectsTest {
+
+    @Test
+    void shouldReturn1AsIsA() {
+        assertEquals("1",fizzBuzz(1));
+        assertEquals("2",fizzBuzz(2));
+        assertEquals("fizz",fizzBuzz(3));
+        assertEquals("fizz",fizzBuzz(6));
+        assertEquals("fizz",fizzBuzz(9));
+        assertEquals("buzz",fizzBuzz(5));
+        assertEquals("buzz",fizzBuzz(10));
+        assertEquals("fizzbuzz",fizzBuzz(15));
+    }
+
+    private String fizzBuzz(int input) {
+        if (multiplo_of_three_and_five(input)) {
+            return "fizzbuzz";
+        }
+        if (multiplo_of_three(input)) {
+            return "fizz";
+        }
+        if (multiplo_of_five(input)) {
+            return "buzz";
+        }
+        return String.valueOf(input);
+    }
+
+    private boolean multiplo_of_three(int input) {
+        return input % 3 == 0;
+    }
+
+    private boolean multiplo_of_five(int input) {
+        return input % 5 == 0;
+    }
+
+    private boolean multiplo_of_three_and_five(int input) {
+        return input % 3 == 0 && input % 5 == 0;
+    }
+}
