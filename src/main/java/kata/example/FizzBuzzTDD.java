947b317b2d11971a5b3d8fad864e5a9bea68513a
aitorevi <aitorevi@gmail.com>
2023-03-08 11:18:50 +0100

    Before second Refactiring add methods

--- src/main/java/kata/example/FizzBuzzTDD.java
+++ src/main/java/kata/example/FizzBuzzTDD.java
@@ -1,18 +1,40 @@
 package kata.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzzTDD  {
 
     public static String fizzBuzz(int number){
-        if (number % 15 == 0){
-            return "FizzBuzz";
+        if (number % 15 == 0) {
+            List<String> results = new ArrayList<>();
+            results.add("FizzBuzz");
+            return results.get(0);
         }
-        if (number % 3 == 0){
-            return "Fizz";
+        if (number % 3 == 0) {
+            List<String> results = new ArrayList<>();
+            results.add("Fizz");
+            return results.get(0);
         }
-        if (number % 5 == 0){
-            return "Buzz";
+        if (number % 5 == 0) {
+            List<String> results = new ArrayList<>();
+            results.add("Buzz");
+            return results.get(0);
         }
-        return String.valueOf(number);
+        List<String> results = new ArrayList<>();
+        results.add(String.valueOf(number));
+        return results.get(0);
     }
 
+    public static List creation_list_of_results(String option){
+
+    }
+/*
+    public static List list_of_results(FizzBuzzTDD.fizzBuzz(10)) {
+        List<String> results = new ArrayList<>();
+
+        return null;
+    }
+
+*/
 }
