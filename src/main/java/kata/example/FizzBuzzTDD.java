70466c206e6eff00d6dfc810973d60c4e8ce6518
aitorevi <aitorevi@gmail.com>
2023-03-08 11:37:04 +0100

    After the second Refactiring added method that creates the result list, only one item

--- src/main/java/kata/example/FizzBuzzTDD.java
+++ src/main/java/kata/example/FizzBuzzTDD.java
@@ -6,35 +6,18 @@ import java.util.List;
 public class FizzBuzzTDD  {
 
     public static String fizzBuzz(int number){
-        if (number % 15 == 0) {
-            List<String> results = new ArrayList<>();
-            results.add("FizzBuzz");
-            return results.get(0);
-        }
-        if (number % 3 == 0) {
-            List<String> results = new ArrayList<>();
-            results.add("Fizz");
-            return results.get(0);
-        }
-        if (number % 5 == 0) {
-            List<String> results = new ArrayList<>();
-            results.add("Buzz");
-            return results.get(0);
-        }
+        if (number % 15 == 0)
+            return creation_list_of_results("FizzBuzz").get(0);
+        if (number % 3 == 0)
+            return creation_list_of_results("Fizz").get(0);
+        if (number % 5 == 0)
+            return creation_list_of_results("Buzz").get(0);
+        return creation_list_of_results(String.valueOf(number)).get(0);
+    }
+    public static List<String> creation_list_of_results(String option){
         List<String> results = new ArrayList<>();
-        results.add(String.valueOf(number));
-        return results.get(0);
+        results.add(option);
+        return results;
     }
 
-    public static List creation_list_of_results(String option){
-
-    }
-/*
-    public static List list_of_results(FizzBuzzTDD.fizzBuzz(10)) {
-        List<String> results = new ArrayList<>();
-
-        return null;
-    }
-
-*/
 }
