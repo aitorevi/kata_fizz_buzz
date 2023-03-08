ed6ceaf39a09bb41c931ef9d5c616c1e1cc4049f
aitorevi <aitorevi@gmail.com>
2023-03-08 10:37:10 +0100

    Before Refactiring

--- src/main/java/kata/example/FizzBuzzTDD.java
+++ src/main/java/kata/example/FizzBuzzTDD.java
@@ -1,4 +1,18 @@
 package kata.example;
 
-public class FizzBuzzTDD {
+public class FizzBuzzTDD  {
+
+    public static String fizzBuzz(int number){
+        if (number % 15 == 0){
+            return "FizzBuzz";
+        }
+        if (number % 3 == 0){
+            return "Fizz";
+        }
+        if (number % 5 == 0){
+            return "Buzz";
+        }
+        return String.valueOf(number);
+    }
+
 }
--- src/test/java/kata/example/FizzBuzzTDDTest.java
+++ src/test/java/kata/example/FizzBuzzTDDTest.java
@@ -1,21 +1,52 @@
 package kata.example;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*
- * 1 -> 1
- * 3 -> Fizz
- * 5 -> Buzz
+ * 1 -> 1 *
+ * 3 -> Fizz *
+ * 5 -> Buzz *
  * 15 -> FizzBuzz
  * number % 3 == 0 -> Fizz
  * number % 5 == 0 -> Buzz
  * number % 15 == 0 -> FizzBuzz
  */
 
-import org.junit.jupiter.api.Test;
-
 public class FizzBuzzTDDTest {
     @Test
-    void should_return_one_as_is_one() {
+    void should_return_one_when_it_is_one() {
+        assertEquals("1",FizzBuzzTDD.fizzBuzz(1));
+    }
+    /*
+    @Test
+    void should_return_Fizz_when_it_is_three() {
+        assertEquals("Fizz",FizzBuzzTDD.fizzBuzz(3));
+    }
 
+    @Test
+    void should_return_Buzz_when_it_is_five() {
+        assertEquals("Buzz",FizzBuzzTDD.fizzBuzz(5));
+    }
+
+    @Test
+    void should_return_FizzBuzz_when_it_is_fifteen() {
+        assertEquals("FizzBuzz",FizzBuzzTDD.fizzBuzz(15));
+    }
+    */
+    @Test
+    void should_return_Fizz_when_it_is_multiplo_of_three() {
+        assertEquals("Fizz",FizzBuzzTDD.fizzBuzz(9));
+    }
+
+    @Test
+    void should_return_Fizz_when_it_is_multiplo_of_five() {
+        assertEquals("Buzz",FizzBuzzTDD.fizzBuzz(10));
+    }
+
+    @Test
+    void should_return_Fizz_when_it_is_multiplo_of_three_and_five() {
+        assertEquals("FizzBuzz",FizzBuzzTDD.fizzBuzz(30));
     }
 
 }
